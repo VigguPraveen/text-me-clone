18d088bbab95e174bb3e119f45041ff5ceaf822d
vigneshbhandarkar <122515543+vigneshbhandarkar@users.noreply.github.com>
2023-09-04 17:30:53 +0530

    added tabs

--- src/components/ListPage/PageController.js
+++ src/components/ListPage/PageController.js
@@ -2,11 +2,13 @@ import React from 'react';
 import classes from '../ListPage/styles/pageController.module.css'
 import { Box } from '@mui/material';
 import ProfilePicture from '../Profile/ProfilePicture';
+import SwitchTabs from '../Tabs/SwitchTabs';
 
 function PageController(props) {
     return (
         <Box className={classes.pageControlMain} >
             <ProfilePicture />
+            <SwitchTabs />
         </Box>
     );
 }
---
+++ src/components/Tabs/SwitchTabs.js
@@ -0,0 +1,21 @@
+import { Tab, Tabs } from '@mui/material';
+import React, { useState } from 'react';
+import classes from './styles/tabs.module.css'
+
+function SwitchTabs(props) {
+
+    const [tabs, setTabs] = useState(0)
+
+    const switchTabHandler = (event, value) => {
+        setTabs(value)
+    }
+
+    return (
+        <Tabs className={classes.tabs} value={tabs} onChange={switchTabHandler}>
+            <Tab  label="Chats" />
+            <Tab  label="Contacts" />
+        </Tabs>
+    );
+}
+
+export default SwitchTabs;
